81421a27cadaa6791a2199889f42f6185541b62b
AlduinsDoom110 <stonefurthefurry@gmail.com>
2025-06-03 12:14:55 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,41 @@
++from board import Board, Move
++from engine import find_best_move
++
++
++def parse_move(move_str: str) -> int:
++    file = ord(move_str[0]) - ord('a')
++    rank = int(move_str[1]) - 1
++    return rank * 8 + file
++
++
++def move_to_str(move_from: int, move_to: int) -> str:
++    fr = chr(ord('a') + move_from % 8) + str(move_from // 8 + 1)
++    to = chr(ord('a') + move_to % 8) + str(move_to // 8 + 1)
++    return fr + to
++
++
++def main():
++    board = Board()
++    depth = 2
++    while True:
++        print(board.get_fen())
++        if board.turn == 'w':
++            user_move = input('Enter your move (e.g., e2e4): ')
++            if len(user_move) < 4:
++                break
++            frm = parse_move(user_move[:2])
++            to = parse_move(user_move[2:4])
++            board.push(Move(frm, to))
++        else:
++            best = find_best_move(board, depth)
++            if best is None:
++                print('No moves available.')
++                break
++            board.push(best)
++            print(f'Engine move: {move_to_str(best.from_square, best.to_square)}')
++
++
++if __name__ == '__main__':
++    main()
++
++
